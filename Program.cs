f008be93439e4eabedaefb12983c8fd3e5ab954b
joshua juliandika <harsenaargrtya1@gmail.com>
2023-03-21 11:45:53 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Taxi taxi = new Taxi();
+            Taxi taxi1 = new Taxi();
+
+            taxi.drivername = "Harsena";
+            taxi.onduty = true;
+            taxi.numpass = 2211;
+
+            taxi1.drivername = "Argretya";
+            taxi1.onduty = true;
+            taxi1.numpass = 4958;
+
+            taxi.info();
+            taxi.ongoing();
+            taxi.done();
+
+            taxi1.info();
+            taxi1.ongoing();
+            taxi1.done();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        public string drivername { get; set; }
+        public Boolean onduty { get; set; }
+        public int numpass { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("\n Taxi Information \n");
+            Console.WriteLine("Nama Driver : {0}", drivername);
+
+            if (onduty == true)
+            {
+                Console.WriteLine("On Work : Yes", onduty);
+                Console.WriteLine("Nomor Pelanggan : {0}", numpass);
+            }
+            else
+            {
+                Console.WriteLine("On Work : No", onduty);
+                Console.WriteLine("Nomor Pelanggan : {0}", numpass);
+            }
+
+
+
+        }
+
+        public void ongoing()
+        {
+            Console.WriteLine("{0} Sedang Membawa Penumpang", drivername);
+        }
+
+        public void done()
+        {
+            Console.WriteLine("{0} Tidak Membawa Penumpang\n\n", drivername);
+        }
+    }
+}
